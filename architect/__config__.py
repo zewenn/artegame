944b4799c7d68a076d61d12fbb386e9be995547e
unknown <takaczsoli@gmail.com>
2024-05-07 00:25:45 +0200

    reordered __config__.py

--- architect/__config__.py
+++ architect/__config__.py
@@ -1,3 +1,12 @@
+# For PyInstaller
+PROJECT_NAME = "Artegame"
+VERSION = "1.2.1"
+
+# For the installer, to install...
+DEPENDENCIES = ["pygame", "termcolor", "recordclass", "zenyx", "result", "PyInstaller"]
+
+# For architect to run the correct __main__.py file
+MAIN_FILE_DIR = "brass"
 
 # Used to store the output of b64encoder.py and import_generator.py
 SERIALISED_OUTPUT_DIR = ["brass", "src"]
@@ -8,13 +17,3 @@ ASSETS_FILE_NAME = "b64_asset_ref_table.py"
 # Used to generate script imports
 ROUTINE_PATH = ["brass", "routines"]
 ROUTINE_IMPORT_FILE_NAME = "imports.py"
-
-# For PyInstaller
-PROJECT_NAME = "Artegame"
-VERSION = "1.2.1"
-
-# For the installer, to install...
-DEPENDENCIES = ["pygame", "termcolor", "recordclass", "zenyx", "result", "PyInstaller"]
-
-# For architect to run the correct __main__.py file
-MAIN_FILE_DIR = "brass"
