686a12825a73153231c36517c6296ae9762f60e0
unknown <takaczsoli@gmail.com>
2024-05-16 22:25:31 +0200

    Builder made nicer

--- brass/src/imports.py
+++ brass/src/imports.py
@@ -2,6 +2,6 @@
 
 # Importing scripts, so they can run
 
-from src.temp import r62341602_default_index_True 
-from src.temp import r66901319_default_player_handler_False 
-from src.temp import r93527102_default___config___False
+from src.temp import r13183700_default___config___rt 
+from src.temp import r41406381_default_index_ui 
+from src.temp import r42786666_default_player_handler_rt
