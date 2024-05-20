f6de9cc679ed79531dd70f4ccda37ffd875ac8aa
unknown <takaczsoli@gmail.com>
2024-05-20 17:49:17 +0200

    gitignore updated

--- brass/src/imports.py
+++ brass/src/imports.py
@@ -2,6 +2,6 @@
 
 # Importing scripts, so they can run
 
-from src.temp import r13183700_default___config___rt 
-from src.temp import r41406381_default_index_ui 
-from src.temp import r42786666_default_player_handler_rt
+from src.temp import r91484369_default___config___rt 
+from src.temp import r93120725_default_index_ui 
+from src.temp import r95498766_default_player_handler_rt
